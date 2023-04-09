eb7a2c2455a8c7c54682bc4212f97e665ed36721
kiwik98 <kiwik9811@gmail.com>
2023-04-09 16:01:20 +0900

    컨테이너에 등록된 모든 빈 조회

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -24,7 +24,6 @@ public class ApplicationContextInfoTest {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
             //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
             //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
              if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
